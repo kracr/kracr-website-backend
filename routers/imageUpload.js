555189c508264cbbbaa03c53d17dc4601fc1ba0e
Utkarsh dubey <dubeyutkarsh2000@gmail.com>
2022-04-24 20:30:12 +0530

    images

--- routers/imageUpload.js
+++ routers/imageUpload.js
@@ -6,6 +6,18 @@ const multer = require("multer");
 const path = require("path");
 var crypto = require("crypto");
 require('dotenv/config');
+var fs = require('fs');
+var request = require('request');
+const Team = require('../modals/team');
+
+var download = function(uri, filename, callback){
+  request.head(uri, function(err, res, body){
+    console.log('content-type:', res.headers['content-type']);
+    console.log('content-length:', res.headers['content-length']);
+    request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+  });
+};
+
 
 var storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -21,8 +33,7 @@ const upload = multer({
 });
 
 imageRouter.post('/upload',upload.single('image'),(req,res)=>{
-    console.log(req);
-    res.send({test:req.file.filename.substring(0,req.file.filename.length-4)});
+    res.send({name:req.file.filename.substring(0,req.file.filename.length-4)});
 });
 
 imageRouter.get('/:id',(req,res)=>{
@@ -30,5 +41,39 @@ imageRouter.get('/:id',(req,res)=>{
     res.sendFile(`/${req.params.id}.jpg`,{root:process.env.imageDir});
 });
 
+imageRouter.get('/test/test',(req,res)=>{
+
+    Team.find({}).then((team)=>{
+        console.log(team);
+        team.forEach((i,index)=>{
+            if(i.ImageURL){
+            console.log("url",i.ImageURL);
+            download(i.ImageURL, `${i.Name}.jpg`, async function(){
+                var bodyFormData = new URLSearchParams();
+                console.log("download done");
+                bodyFormData.append('image',fs.createReadStream(`A:/Semester 6/kracr-website-backend/${i.Name}.jpg`)); 
+                axios({
+                    method: "post",
+                    url: "http://localhost:5000/image/upload",
+                    data: bodyFormData,
+                   
+                  }).then(async(data)=>{
+                      console.log("upload done");
+                        await Team.findByIdAndUpdate(i._id,{$set:{ImageURL:data.name}});
+                  });
+            });
+        }
+        else{
+            console.log("no image",i);
+        }
+        });
+
+        // download('https://www.google.com/images/srpr/logo3w.png', 'check.png', function(){
+
+        // });
+    })
+    
+    // res.send({done:"done"});
+})
 
 module.exports = imageRouter;
